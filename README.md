1bfb4c3921355514700bc4d68746acee20f2e918
ericdouglasb <146323513+ericdouglasb@users.noreply.github.com>
2023-11-21 15:58:43 +0100

    update

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# bad_tdd_bank
+# bdd_tdd_bank
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+
+    double[] officeSizes = {42.7, 20.9, 38.7, 24.3, 12.5, 14.8, 14};
+
+    for (var size : officeSizes){
+        Office office = new Office(size);
+        double monthlyRent = office.getMonthlyRent();
+        System.out.println("kostnad för " + size + " kvm kontor: " + monthlyRent + " kr per månad");
+        }
     }
+
+
+
+
+
+
 }
